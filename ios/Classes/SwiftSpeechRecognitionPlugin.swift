bd64a7dcfab62f1de7088f1c1134d892877d2925
Crizant Lai <crizant@whiletrue.codes>
2019-09-25 16:58:56 +0800

    set AvAudioSession category to playAndRecord

--- ios/Classes/SwiftSpeechRecognitionPlugin.swift
+++ ios/Classes/SwiftSpeechRecognitionPlugin.swift
@@ -108,7 +108,7 @@ public class SwiftSpeechRecognitionPlugin: NSObject, FlutterPlugin, SFSpeechReco
     cancelRecognition(result: nil)
 
     let audioSession = AVAudioSession.sharedInstance()
-    try audioSession.setCategory(AVAudioSession.Category.record, mode: .default)
+    try audioSession.setCategory(AVAudioSession.Category.playAndRecord, mode: .default)
     try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
 
     recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
